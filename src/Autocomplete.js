6ec5bf82aaa8ec5547b74c1402270caaabbfa897
Shubham Shah <shubhamshahrising@gmail.com>
2022-04-28 11:03:00 +0530

    Add a theme from algolia

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,4 +1,5 @@
 import { autocomplete } from "@algolia/autocomplete-js";
+import "@algolia/autocomplete-theme-classic";
 import { useEffect, useRef } from "react";
 
 export function Autocomplete(props) {
@@ -12,6 +13,7 @@ export function Autocomplete(props) {
     // Initialize autocomplete on the newly created container
     const search = autocomplete({
       container: containerRef.current,
+      placeholder: "Search",
       ...props,
     });
     // Destroy the search instance in cleanup
